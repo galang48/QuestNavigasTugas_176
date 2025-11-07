873b1e644236aea1d163df575d816345bc02a47f
galang48 <galangyudha1945@gmail.com>
2025-11-07 23:26:45 +0700

    Menambahkan composable WelcomeScreen dengan layout Surface+Column dan padding_medium

--- app/src/main/java/com/example/inputnavigasi/view/WelcomeScreen.kt
+++ app/src/main/java/com/example/inputnavigasi/view/WelcomeScreen.kt
@@ -1,2 +1,32 @@
 package com.example.inputnavigasi.view
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.dimensionResource
+import com.example.inputnavigasi.R
+import androidx.compose.ui.Alignment
+
+@Composable
+fun WelcomeScreen(onMasuk: () -> Unit) {
+    Surface(modifier = Modifier.fillMaxSize()) {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(dimensionResource(id = R.dimen.padding_medium)),
+            verticalArrangement = Arrangement.SpaceBetween,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+
+
+            }
+        }
+    }
+
+
